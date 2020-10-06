3ae83c410ebb780dac50302e120a85f4211714c9
621-dai <dai.dt621@gmail.com>
2020-10-06 17:32:38 +0900

    mailの正規表現_修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,7 @@ class User < ApplicationRecord
                          message: 'is must NOT contain any other characters than alphanumerics.'} # 6文字以上の英数字のみ可
     validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, 
                message: "is invalid. Input full-width katakana characters."} # カタカナのみ使用可
-    validates :email, presence: true,
-    uniqueness: { case_sensitive: false }, # 重複不可 # @を挟んだemailの形になっているか
-    format: { with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i, 
-    message: 'is must NOT contain any other characters than alphanumerics.' }
+    validates :email, uniqueness: { case_sensitive: false }, # 重複不可 # @を挟んだemailの形になっているか
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'is must NOT contain any other characters than alphanumerics.'}
   end  
 end
