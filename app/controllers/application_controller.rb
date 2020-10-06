5ffca48521f1f2c51397ab28b643a06defbe76a2
621-dai <dai.dt621@gmail.com>
2020-10-06 22:47:34 +0900

    user_controller_rev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-ユーザー管理機能の実装
+class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -7,3 +7,4 @@
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
+
