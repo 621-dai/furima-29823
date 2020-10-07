c918ee3aeec43a43c783d9bd974195e23604ca16
621-dai <dai.dt621@gmail.com>
2020-10-08 00:35:01 +0900

    Active_hash_01

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,25 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
 
+  def create
+    @items = Item.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id)
   end
 
 end
