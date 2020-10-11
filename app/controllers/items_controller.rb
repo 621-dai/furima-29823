212f56e2700a80e7dadaf5a9d321be9bb17e07b7
621-dai <dai.dt621@gmail.com>
2020-10-11 19:40:32 +0900

    current _user_rev

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless current_user == current_user.id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
