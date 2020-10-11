06777f72646a7462d4a39978d186fac0ecf99475
621-dai <dai.dt621@gmail.com>
2020-10-11 18:56:43 +0900

    出品者以外が編集ページにいけない_修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :new, :show]
 
   def index
     @item = Item.all.order("created_at DESC")
@@ -47,6 +48,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? == current_user.id
+      redirect_to action: :index
+    end
   end
 
 end
